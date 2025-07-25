45a969559782d13539c6816dba5329cec46db3c6
Oleksandr Yanytskyi <yanytskyi.oleksandr@gmail.com>
2025-07-26 00:42:28 +0300

    Add project files.

---
+++ BaseTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+
+namespace _Task__Selenium_WebDriver
+{
+    public class BaseTest
+    {
+        protected IWebDriver driver;
+        protected WebDriverWait wait;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.epam.com/");
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+                driver.Dispose();
+            }
+        }
+    }
+}
---
+++ CareersTests.cs
@@ -0,0 +1,66 @@
+﻿using OpenQA.Selenium;
+
+namespace _Task__Selenium_WebDriver
+{
+    public class CareersTests : BaseTest
+    {      
+        [TestCase("Java", "All Locations")]
+        [TestCase("C#", "All Locations")]
+        [TestCase("Python", "All Locations")]
+        public void ValidateJobSearchByCriteria(string language, string locationName)
+        {
+            NavigateToCareers();
+            EnterKeyword(language);
+            SelectLocation(locationName);
+            ToggleRemoteOption();
+            SubmitSearch();
+            OpenLastSearchResult();
+            AssertLanguageIsPresent(language);
+        }
+
+        private void NavigateToCareers()
+        {
+            driver.FindElement(By.LinkText("Careers")).Click();
+        }
+
+        private void EnterKeyword(string language)
+        {
+            var keywordField = driver.FindElements(By.CssSelector(".recruiting-search__keyword"))[1];
+            keywordField.SendKeys(language);
+        }
+
+        private void SelectLocation(string locationName)
+        {
+            var locationDropdown = wait.Until(d => d.FindElement(By.CssSelector(".recruiting-search__location")));
+            locationDropdown.Click();
+
+            var optionToClick = wait.Until(d => d.FindElement(By.XPath($"//li[contains(text(), '{locationName}')]")));
+            optionToClick.Click();
+        }
+
+        private void ToggleRemoteOption()
+        {
+            var remoteElement = driver.FindElement(By.XPath("//input[@name='remote']/.."));
+            remoteElement.Click();
+        }
+
+        private void SubmitSearch()
+        {
+            driver.FindElement(By.CssSelector("#jobSearchFilterForm > button")).Click();
+        }
+
+        private void OpenLastSearchResult()
+        {
+            string lastResultXPath = "//ul[@class='search-result__list']/li[last()]/descendant::a[text()='View and apply']";
+            wait.Until(d => d.FindElements(By.XPath(lastResultXPath)).Count > 0);
+            var link = driver.FindElement(By.XPath(lastResultXPath));
+            link.Click();
+        }
+
+        private void AssertLanguageIsPresent(string language)
+        {
+            bool isPresent = driver.FindElements(By.XPath($"//*[contains(text(), '{language}')]")).Count > 0;
+            Assert.That(isPresent, Is.True, $"Expected programming language '{language}' is not found on the page.");
+        }
+    }
+}
